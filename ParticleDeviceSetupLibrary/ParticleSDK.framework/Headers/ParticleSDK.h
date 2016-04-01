66b739178e2f7f67a5e013581608596f3cf83dcd
Ido Kleinman <idokleinman@gmail.com>
2016-03-31 17:23:48 -0700

    move Cartfile to root dir and re-install deps

---
+++ ParticleDeviceSetupLibrary/ParticleSDK.framework/Headers/ParticleSDK.h
@@ -0,0 +1,20 @@
+//  ParticleSDK.h
+//  ParticleSDK
+//
+//  Created by Ido Kleinman on 2/29/16.
+//  Copyright © 2016 Particle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ParticleSDK.
+FOUNDATION_EXPORT double ParticleSDKVersionNumber;
+
+//! Project version string for ParticleSDK.
+FOUNDATION_EXPORT const unsigned char ParticleSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ParticleSDK/PublicHeader.h>
+#import <ParticleSDK/SparkCloud.h>
+#import <ParticleSDK/SparkDevice.h>
+
+
